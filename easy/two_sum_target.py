fafe883166f1dc6932aa5c4e329b1de2765fa551
muhammadkhanusmanov <uqaxromon@gmail.com>
2025-10-07 12:11:03 +0500

    Two Sum II - Input Array Is Sorted

---
+++ easy/two_sum_target.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        right, left = 0, len(numbers)-1
+        while right<left:
+            new_sum = numbers[left]+numbers[right]
+            if new_sum==target:
+                return [right+1, left+1]
+            if new_sum>target:
+                left-=1
+            else:
+                right+=1
